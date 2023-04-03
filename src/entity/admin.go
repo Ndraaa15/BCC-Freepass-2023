9501dea0fd6f6050b1839543b9a5ca6250bb47de
Gede Indra Adi Brata <indrabrata599@gmail.com>
2023-04-03 22:29:51 +0700

    Create : admin entity

--- src/entity/admin.go
+++ src/entity/admin.go
@@ -1,1 +1,11 @@
 package entity
+
+import "time"
+
+type admin struct {
+	ID        uint      `gorm:"primarykey"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	username  string    `json:"username" binding:"required" gorm:"unique"`
+	password  string    `json:"password" binding:"required`
+}
