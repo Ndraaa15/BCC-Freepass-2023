699bda0bd2ce08c667edee445720517a2537180f
Gede Indra Adi Brata <indrabrata599@gmail.com>
2023-04-03 22:40:03 +0700

    Update : admin entity Create : student entity

--- src/entity/admin.go
+++ src/entity/admin.go
@@ -6,6 +6,6 @@ type admin struct {
 	ID        uint      `gorm:"primarykey"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
-	username  string    `json:"username" binding:"required" gorm:"unique"`
-	password  string    `json:"password" binding:"required`
+	Username  string    `json:"username" binding:"required" gorm:"unique"`
+	Password  string    `json:"password" binding:"required"`
 }
